671da0476163c4f6922b35a3804bac27d682ee12
ajcodes020 <108165407+ajcodes020@users.noreply.github.com>
2024-02-07 10:41:24 +0800

    Add files via upload

---
+++ tip_calculator.py
@@ -0,0 +1,25 @@
+print(r"""
+                         _____________________
+                        |  _________________  |
+  _______ _             | | JO           0. | |
+ |__   __(_)            | |_________________| |
+    | |   _ _ __        |  ___ ___ ___   ___  |
+    | |  | | '_ \       | | 7 | 8 | 9 | | + | |
+    | |  | | |_) |      | |___|___|___| |___| |
+    |_|  |_| .__/       | | 4 | 5 | 6 | | - | |
+           | |          | |___|___|___| |___| |
+           |_|          | | 1 | 2 | 3 | | x | |
+                        | |___|___|___| |___| |
+                        | | . | 0 | = | | / | |
+                        | |___|___|___| |___| |
+                        |_____________________|
+""")
+print("Welcome to the tip calculator.")
+bill = float(input("What was the total bill? $"))
+tip = int(input("What percentage tip would you like to give? 10, 12, or 15? "))
+people = int(input("How many people to split the bill? "))
+
+tip = tip * .01 + 1
+total_bill = "{:.2f}".format(bill / people * tip, 2)
+
+print(f"Each person should pay: ${total_bill}")
