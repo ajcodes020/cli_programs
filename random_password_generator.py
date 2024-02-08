2091a7f9368df5ed876b39e084f5158278b162bf
ajcodes020 <108165407+ajcodes020@users.noreply.github.com>
2024-02-08 11:03:32 +0800

    Update random_password_generator.py

--- random_password_generator.py
+++ random_password_generator.py
@@ -1,21 +1,21 @@
 import random
 
-random_password = r"""                                                                    
+print(r"""                                                                    
  _____           _              _____                             _ 
 | __  |___ ___ _| |___ _____   |  _  |___ ___ ___ _ _ _ ___ ___ _| |
 |    -| .'|   | . | . |     |  |   __| .'|_ -|_ -| | | | . |  _| . |
 |__|__|__,|_|_|___|___|_|_|_|  |__|  |__,|___|___|_____|___|_| |___|
                                                                     
-"""
+""")
 
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the Random Password Generator.")
-nr_letters = int(input("How many letters would you like in your password?\n")) 
-nr_symbols = int(input("How many symbols would you like?\n"))
-nr_numbers = int(input("How many numbers would you like?\n"))
+nr_letters = int(input("How many letters would you like in your password? ")) 
+nr_symbols = int(input("How many symbols would you like? "))
+nr_numbers = int(input("How many numbers would you like? "))
 
 random_password = ""
 # Easy Level - Order not randomized:
