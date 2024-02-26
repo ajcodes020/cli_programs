964a74c902d92fee311e1919517c53cd973e46c8
ajcodes020 <108165407+ajcodes020@users.noreply.github.com>
2024-02-26 17:54:36 +0800

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,1 +1,28 @@
+import random
 
+
+def guess_the_number():
+    """ starts the game """
+    print("Welcome to Guess the Number.")
+    print("A random number between 1 and 100 is generated.")
+    random_number = random.randint(1, 100)
+    difficulty = input("Choose difficulty. Type 'easy' or 'hard': ")
+    remaining_tries = 10 if difficulty == 'easy' else 5
+
+    while remaining_tries > 0:
+        print(
+            f"You have {remaining_tries} attempts remaining to guess the number."
+        )
+        guess = int(input("Make a guess: "))
+        if guess == random_number:
+            print("You win.")
+            break
+        elif guess > random_number:
+            print("Too high.")
+            remaining_tries -= 1
+        else:
+            print("Too low.")
+            remaining_tries -= 1
+
+
+guess_the_number()
