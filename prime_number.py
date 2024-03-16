02cd6ac5031d5522d48028963153f05d851f3dee
ajcodes020 <108165407+ajcodes020@users.noreply.github.com>
2024-03-16 20:06:39 +0800

    Create prime_number.py

---
+++ prime_number.py
@@ -0,0 +1,11 @@
+def prime_checker(number):
+    for i in range(2, number):
+        if number % i == 0:
+            print("It's not a prime number.")
+            break
+    else:
+        print("It's a prime number.")
+
+
+n = int(input()) # Check this number
+prime_checker(number=n)
